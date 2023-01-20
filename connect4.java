80a89ea86f568290cf8e2ae42cf127cb14b2ab65
gabrielsalinas24 <113557530+gabrielsalinas24@users.noreply.github.com>
2023-01-20 10:39:20 -0800

    Add files via upload

---
+++ connect4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class connect4
+{
+    Scanner scanner = new Scanner(System.in);
+    boolean shouldContinue = true;
+    public void runEventLoop() {
+        while(shouldContinue){
+            
+        }
+    }
+    public static void main (String[] args) {
+        connect4 connect = new connect4();
+        connect.runEventLoop();
+    }
+}
